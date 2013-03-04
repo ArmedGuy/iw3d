6529890283e75e16d6fc8750ec06a8ce303cac83
Johan Jatko <johan@pie-studios.com>
2013-03-04 14:10:20 +0100

    Deleted misc file

--- iw3d/PatchCoD4.cpp
+++ iw3d/PatchCoD4.cpp
@@ -33,3 +33,24 @@ void PatchCoD4()
 
 	PatchCoD4_Misc();
 }
+
+void PatchCoD4_Misc()
+{
+
+	// disable "dvar set" string in console log
+	memset((void*)0x56B203, 0x90, 5);
+
+	// remove "WARNING: unknown dvar" from console
+	memset((void*)0x56D61A, 0x90, 5);
+
+	// remove improper quit message
+	char tmp1[] = {0xEB, 0x50};
+	memcpy((void*)0x577415,  tmp1, 2);
+
+	// remove computer changed message
+	char tmp2[] = {0xEB, 0x2F};
+	memcpy((void*)0x576775, tmp2, 2);
+
+	// remove delta request messages(hopefully)
+	//*(char*)0x534180 = 0xEB;
+}
