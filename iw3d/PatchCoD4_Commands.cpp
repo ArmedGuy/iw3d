f8330344acb20fa75ffce0601d7c056f64012c42
win32re <win32re@users.noreply.github.com>
2015-02-11 17:54:08 +0100

    Update PatchCoD4_Commands.cpp

--- iw3d/PatchCoD4_Commands.cpp
+++ iw3d/PatchCoD4_Commands.cpp
@@ -2,7 +2,8 @@
 // iw3d project
 //
 // File: PatchCoD4_Commands.cpp
-// Purpose: Experimenting with hooking existing commands and provide custom functionality
+// Purpose: Experimenting with hooking existing commands
+//          and provide custom functionality
 //
 // Initial author: ArmedGuy
 // ==========================================================
@@ -12,24 +13,16 @@
 CallHook hKickHook;
 CallHook hRemoteKickHook;
 
-void KickCommandHook(char, char);
-
-DWORD pKickCommand = 0x52802E;
-DWORD pRemoteKickCommand = 0x52817B;
-
-
-void PatchCoD4_Commands()
-{
-	hKickHook.initialize(pKickCommand, KickCommandHook);
-	hKickHook.installHook();
-
-	hRemoteKickHook.initialize(pRemoteKickCommand, KickCommandHook);
-	hRemoteKickHook.installHook();
-}
-
-
-
 void KickCommandHook(char d1, char d2)
 {
 	Com_Printf(0, "Usage: kick <player> [reason]\n");
+}
+
+void PatchCoD4_Commands()
+{
+	hKickHook.initialize(0x52802E, KickCommandHook);
+	hKickHook.installHook();
+
+	hRemoteKickHook.initialize(0x52817B, KickCommandHook);
+	hRemoteKickHook.installHook();
 }
